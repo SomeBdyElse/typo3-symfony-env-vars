29950c80341df616fa9745e1ac6af962b7a2a401
Hannes Lau <office@hanneslau.de>
2024-02-20 21:45:48 +0100

    [TASK] Remove misleading comment

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,6 @@ use SomeBdyElse\SymfonyEnvVariables\PlaceholderProcessor\EnvironmentVariablePlac
 
 defined('TYPO3_MODE') or die();
 
-// Add processor to replace dynamic database record uid in the site configuration
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['yamlLoader']['placeholderProcessors'][EnvironmentVariablePlaceholderProcessor::class] = [
     'before' => [
         \TYPO3\CMS\Core\Configuration\Processor\Placeholder\EnvVariableProcessor::class,
